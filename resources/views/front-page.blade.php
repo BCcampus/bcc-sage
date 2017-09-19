7ab7dfe34f93f2c793f8cc6d33edec91da2fe0e5
bdolor <bpayne@bccampus.ca>
2017-09-19 15:22:12 -0700

    initial commit bcc-sage

---
+++ resources/views/front-page.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+  @debug('hierarchy')
+  @debug('controller')
+  @debug('dump')
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    @include('partials.content-page')
+  @endwhile
+@endsection
