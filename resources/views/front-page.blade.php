33d308967f37e745c4dc382e4ddda0f77c2181c2
bdolor <bpayne@bccampus.ca>
2017-09-21 21:09:46 -0700

    fixing lint config error, add common style elements

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  @debug('hierarchy')
-  @debug('controller')
-  @debug('dump')
+  {{--@debug('hierarchy')--}}
+  {{--@debug('controller')--}}
+  {{--@debug('dump')--}}
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
     @include('partials.content-page')
