c1905d17fdc53ca7c1cf311bffba0704e4a06231
bdolor <bpayne@bccampus.ca>
2017-10-01 21:03:00 -0700

    add uio plugin

---
+++ resources/views/partials/uio-script.blade.php
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+  $(document).ready(function () {
+    fluid.uiOptions.prefsEditor(".flc-prefsEditor-separatedPanel", {
+      tocTemplate: "/wp-content/themes/bcc-sage/lib/infusion/src/components/tableOfContents/html/TableOfContents.html",
+      terms: {
+        templatePrefix: "/wp-content/themes/bcc-sage/lib/infusion/src/framework/preferences/html",
+        messagePrefix: "/wp-content/themes/bcc-sage/lib/infusion/src/framework/preferences/messages"
+      }
+    });
+  })
+</script>
