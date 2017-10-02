5b4715c916d3a1f3d0b39a905d2d0110840b155f
bdolor <bpayne@bccampus.ca>
2017-10-02 14:45:06 -0700

    install infusion 2.0 with minimal functionality

--- resources/views/partials/uio-script.blade.php
+++ resources/views/partials/uio-script.blade.php
@@ -1,5 +1,9 @@
 <script type="text/javascript">
   $(document).ready(function () {
+    // table of contents
+    var tocPlaceholder = "<div class='flc-toc-tocContainer toc'> </div>";
+    $('main').prepend(tocPlaceholder);
+
     fluid.uiOptions.prefsEditor(".flc-prefsEditor-separatedPanel", {
       tocTemplate: "/wp-content/themes/bcc-sage/lib/infusion/src/components/tableOfContents/html/TableOfContents.html",
       terms: {
