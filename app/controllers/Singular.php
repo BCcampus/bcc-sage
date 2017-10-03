d3b3e8033f016047f4c72975b865dba2d14409cb
bdolor <bpayne@bccampus.ca>
2017-10-03 11:48:15 -0700

    ancestor based nav on pages

--- app/controllers/Singular.php
+++ app/controllers/Singular.php
@@ -6,6 +6,19 @@ use Sober\Controller\Controller;
 
 class Singular extends Controller
 {
+    /**
+     * Get ID of the parent at the top of the tree
+     *
+     * @param $id
+     *
+     * @return mixed
+     */
+    public static function getParentId( $id ) {
 
+    $ancestors = get_post_ancestors( $id );
+
+    return array_pop($ancestors);
+
+    }
 
 }
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,19 +1,19 @@
 @if(is_page())
-{!! wp_list_pages( [
-'depth'        => -1,
-'show_date'    => '',
-'date_format'  => get_option( 'date_format' ),
-'child_of'     => $post->post_parent,
-'exclude'      => '',
-'title_li'     => '<a href="'.get_the_permalink($post->post_parent).'">'. get_the_title($post->post_parent) . '</a>',
-'echo'         => 0,
-'authors'      => '',
-'sort_column'  => 'post_parent',
-'link_before'  => '',
-'link_after'   => '',
-'item_spacing' => 'preserve',
-'walker'       => '',
-] ); !!}
+  {!! wp_list_pages( [
+  'depth'        => -1,
+  'show_date'    => '',
+  'date_format'  => get_option( 'date_format' ),
+  'child_of'     => \App\Singular::getParentId( $post->ID ),
+  'exclude'      => '',
+  'title_li'     => '<a href="' . get_the_permalink( \App\Singular::getParentId( $post->ID ) ) . '">' . get_the_title( \App\Singular::getParentId( $post->ID ) ) . '</a>',
+  'echo'         => 0,
+  'authors'      => '',
+  'sort_column'  => 'post_parent',
+  'link_before'  => '',
+  'link_after'   => '',
+  'item_spacing' => 'preserve',
+  'walker'       => '',
+  ] ); !!}
 @endif
 
 @php(dynamic_sidebar('sidebar-primary'))
