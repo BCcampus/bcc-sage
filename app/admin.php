a50d302b59e1e9372261dc02ff45c2b9b9c4a668
Brad <brad@bradpayne.ca>
2017-10-04 12:44:30 -0700

    remove redundant editor stylesheet (#41)

--- app/admin.php
+++ app/admin.php
@@ -23,9 +23,3 @@ add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
 
-/**
- * Editor Stylesheet
- */
-add_action( 'admin_init', function (){
-    add_editor_style( 'dist/styles/editor-style.css' );
-});
