8d2fead4e20e706f84cb7fc546235e97f65202bc
Brad <brad@bradpayne.ca>
2017-10-06 13:19:11 -0700

    Events (#46)
    * Different nav text for events
    
    * events list
    
    * get date cloodge

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -13,6 +13,18 @@
     <div itemprop="articleBody" class="entry-content">
         @php(the_content())
     </div>
+    @if($get_upcoming_events)
+        <hr>
+        <div class="upcoming-events" itemscope itemtype="http://schema.org/Event">
+            <p>Join us at an upcoming event:</p>
+          <ul>
+            @foreach($get_upcoming_events as $upcoming_event )
+              <li><a itemprop="url" href="{{$upcoming_event['link']}}" title="Permanent Link to {{$upcoming_event['title']}}"><span itemprop="name">{{$upcoming_event['title']}}</span></a> — <span itemprop="startDate">{{$upcoming_event['start']}}</span></li>
+          @endforeach
+          </ul>
+        </div>
+        <hr>
+    @endif
     <p class="byline author vcard">
         {{ __('Posted by', 'bcc-sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author"
                                              class="fn"><span itemprop="author" itemscope itemtype="http://schema.org/Person"><span itemprop="name">{{ get_the_author() }}</span></span></a> &amp; filed
@@ -23,12 +35,19 @@
         {!! wp_link_pages(['echo' => 1, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'bcc-sage'), 'after' => '</p></nav>']) !!}
         <nav>
             <ul class="clearfix">
-                <li class="post-navigation pull-left col-6">{!! previous_post_link('&laquo; Previous Article<br>%link') !!}</li>
-                <li class="post-navigation pull-right text-right col-6">{!! next_post_link('Next Article &raquo;<br>%link ') !!} </li>
+                @if( is_singular( 'ai1ec_event' ))
+                    <li class="post-navigation pull-left col-6">{!! previous_post_link('&laquo; Previous Event<br>%link') !!} </li>
+                    <li class="post-navigation pull-right text-right col-6">{!! next_post_link('Next Event &raquo;<br>%link ') !!} </li>
+                @else
+                    <li class="post-navigation pull-left col-6">{!! previous_post_link('&laquo; Previous Article<br>%link') !!} </li>
+                    <li class="post-navigation pull-right text-right col-6">{!! next_post_link('Next Article &raquo;<br>%link ') !!} </li>
+                @endif
             </ul>
         </nav>
-        <hr>
-        <p class="text-center">Related Articles</p>
+        @if( !is_singular( 'ai1ec_event' ))
+            <p class="text-center">Related Articles</p>
+            <hr>
+        @endif
         @if($get_related_posts)
             @foreach($get_related_posts as $related_post )
                 <a href="{{$related_post->guid}}" rel="bookmark"
