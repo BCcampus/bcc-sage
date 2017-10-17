c4288526253ca412601aee1629b977daa763d26b
bdolor <bpayne@bccampus.ca>
2017-10-17 10:56:36 -0700

    limit depth of nav, change title logic, pronounced indentation

--- app/controllers/Singular.php
+++ app/controllers/Singular.php
@@ -5,25 +5,6 @@ namespace App;
 use Sober\Controller\Controller;
 
 class Singular extends Controller {
-    /**
-     * Get ID of the parent at the top of the tree
-     *
-     * @param $id
-     *
-     * @return mixed
-     */
-    public static function getParentId( $id ) {
 
-        // see if the ID has a parent
-        $parent_id = wp_get_post_parent_id( $id );
-
-        if ( false == $parent_id ) {
-            $ancestors = get_post_ancestors( $id );
-            $parent_id = array_pop( $ancestors );
-        }
-
-        return $parent_id;
-
-    }
 
 }
