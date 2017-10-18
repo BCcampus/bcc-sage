eff654b8d73d8fc39e5b66aab467305553d1c892
bdolor <bpayne@bccampus.ca>
2017-10-18 13:58:17 -0700

    alphabetical page order

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
   'title_li'     => \App\App::getListHeading( $post->ID ),
   'echo'         => 0,
   'authors'      => '',
-  'sort_column'  => 'menu_order,post_parent',
+  'sort_column'  => 'post_title',
   'link_before'  => '',
   'link_after'   => '',
   'item_spacing' => 'preserve',
