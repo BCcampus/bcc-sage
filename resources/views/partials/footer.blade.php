3429ece088857c8e24f30b8e435b6379bee91c40
bdolor <bpayne@bccampus.ca>
2017-10-18 11:15:15 -0700

    updating style of footer-link navigation

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -27,7 +27,7 @@
           </small>
         </p>
         @if(has_nav_menu('footer_navigation_3'))
-          <nav class="footer-links menu widget">
+          <nav class="footer-links horizontal">
             {!! wp_nav_menu( ['theme_location' => 'footer_navigation_3', 'menu' => 'Footer Horizontal Navigation', 'menu_class' => 'menu', 'link_after' => '&nbsp;|&nbsp;', 'depth' => 0 ] ) !!}
           </nav>
         @endif
