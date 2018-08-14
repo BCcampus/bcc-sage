45096da6a52313b2c9bb252a35f3a6b124082183
bdolor <bpayne@bccampus.ca>
2018-08-14 14:12:09 -0700

    add topic of practice partial placeholder

--- resources/views/template-topics-of-practice.blade.php
+++ resources/views/template-topics-of-practice.blade.php
@@ -7,6 +7,7 @@
 @section('content')
 	@while(have_posts()) @php(the_post())
 	@include('partials.content-page')
+	@include('partials.topics-of-practice')
 	@include('partials.related-news')
 	@include('partials.related-events')
 	@endwhile
