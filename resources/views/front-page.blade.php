a66c13966ab85cbaa1c807e44cc865bfa057e88b
bdolor <bpayne@bccampus.ca>
2018-08-14 14:47:00 -0700

    fix search blade, modify front page with placeholders

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.front')
 
 @section('content')
-  <span itemscope itemtype="http://schema.org/ItemPage">
-  @include('partials.brochure')
-  @include('partials.projects')
+	<span itemscope itemtype="http://schema.org/ItemPage">
+	  <div class="row">
+	  <div class="col-4">
+	  @include('partials.children-top')
+		  @include('partials.children-projects')
+	  </div>
+	  <div class="col-8">
+	  @include('partials.news-feature')
+		  @include('partials.news-recent')
+		  @include('partials.events-feature')
+		  @include('partials.events-upcoming')
+	  </div>
+	  </div>
   <span>
 @endsection
---
+++ resources/views/partials/events-feature.blade.php
@@ -0,0 +1,11 @@
+<article class="feature-events">
+	<h4>Feature Events</h4>
+	<section class="row articles">
+		<article class="col-sm">
+			Placeholder
+		</article>
+		<article class="col-sm">
+			Placeholder
+		</article>
+	</section>
+</article>
