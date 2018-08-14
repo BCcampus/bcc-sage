ac3593dbc95d7038f07d41fa3743523211797153
bdolor <bpayne@bccampus.ca>
2018-08-14 15:52:18 -0700

    add page children function

--- resources/views/partials/children-projects.blade.php
+++ resources/views/partials/children-projects.blade.php
@@ -1,15 +1,20 @@
 <article class="current-projects">
 	<h4>Projects</h4>
+	@foreach($get_children_of_page as $child)
 		<article class="col-sm">
-			Placeholder
+			<p>{{$child->post_title}}</p>
 		</article>
-		<article class="col-sm">
-			Placeholder
-		</article>
-		<article class="col-sm">
-			Placeholder
-		</article>
-		<article class="col-sm">
-			Placeholder
-		</article>
+	@endforeach
+	<article class="col-sm">
+		Placeholder
+	</article>
+	<article class="col-sm">
+		Placeholder
+	</article>
+	<article class="col-sm">
+		Placeholder
+	</article>
+	<article class="col-sm">
+		Placeholder
+	</article>
 </article>
