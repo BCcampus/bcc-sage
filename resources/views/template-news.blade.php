1b971d83fb12209f33b192b51beeff943ffd345e
bdolor <bpayne@bccampus.ca>
2018-08-15 13:20:14 -0700

    wider columns with full width template

--- resources/views/template-news.blade.php
+++ resources/views/template-news.blade.php
@@ -7,10 +7,10 @@
 @section('content')
 	@while(have_posts()) @php(the_post())
 	<div class="row">
-	<div class="col-sm-4">
+	<div class="col-sm-6">
 		@include('partials.news-feature')
 	</div>
-	<div class="col-sm-4">
+	<div class="col-sm-6">
 		@include('partials.subscribe')
 		@include('partials.news-recent')
 	</div>
