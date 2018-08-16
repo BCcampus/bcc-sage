cb9e4cc163c4f8bbc69db9b295b64c6552f4aadc
bdolor <bpayne@bccampus.ca>
2018-08-16 13:21:48 -0700

    topics of practice child pages on template and styled

--- resources/views/partials/children-top.blade.php
+++ resources/views/partials/children-top.blade.php
@@ -1,14 +1,20 @@
 <section class="topics-of-practice">
-	<h4 class="text-capitalize">Topics of Practice</h4>
-	<ul>
-		@foreach(\App\Page::getChildrenOfPage($get_topics_of_practice_id) as $child)
+	<div class=" d-flex flex-row flex-wrap">
+		@foreach(\App\Page::getChildrenOfPage() as $child)
 			<?php
 			// not using $child->guid since guid does not
 			// update to current domain when importing content
 			$link = site_url() . '/' . $child->post_name;
-			;
-			?>
-			<li><a class="purple" href="<?php echo $link; ?>">{{$child->post_title}}</a></li>
+			;?>
+			<article class="col-sm-6 t-o-p">
+				<div class="featured-image-box">
+					<a href="<?php echo $link; ?>"><?php echo get_the_post_thumbnail( $child->ID, [ 400 ] ); ?></a>
+					<p></p>
+				</div>
+				<h5 class="purple-bkgd"><a class="text-white" href="<?php echo $link; ?>">{{$child->post_title}}</a>
+				</h5>
+				<p><?php echo wp_trim_words( $child->post_content, '30', "<a href='{$link}'>&hellip;</a>" ); ?></p>
+			</article>
 		@endforeach
-	</ul>
+	</div>
 </section>
