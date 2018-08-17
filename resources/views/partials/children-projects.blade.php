b519baddb43bbecb3c2a219b1e4b680440014cf2
bdolor <bpayne@bccampus.ca>
2018-08-17 15:44:24 -0700

    fix projects listing

--- resources/views/partials/children-projects.blade.php
+++ resources/views/partials/children-projects.blade.php
@@ -6,7 +6,12 @@
 		$link = site_url() . '/' . $child->post_name;
 		;?>
 		<article class="projects d-flex flex-row">
-			<a class="p-2" href="<?php echo $link; ?>"><?php echo \App\App::getThumb( $child->ID, [ 175, 175 ] ); ?></a>
+			<div class="featured-image-box side-img p-2">
+				<a href="<?php echo $link; ?>"><?php echo \App\App::getThumb( $child->ID, [
+						175,
+						175
+					] ); ?></a>
+			</div>
 			<div class="p-2">
 				<h5><a class="purple" href="<?php echo $link; ?>">{{$child->post_title}}</a></h5>
 				<p><?php echo wp_trim_words( $child->post_content, '30', "<a href='{$link}'>&hellip;</a>" ); ?></p>
