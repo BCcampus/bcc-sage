05ae13ebbf16a84113aac30e940ca031e65943f6
bdolor <bpayne@bccampus.ca>
2018-08-17 15:48:01 -0700

    create related events partial

--- resources/views/partials/events-related.blade.php
+++ resources/views/partials/events-related.blade.php
@@ -1,17 +1,6 @@
-<article class="related-events">
-	<h4>Related Events</h4>
-	<section class="row articles">
-		<article class="col-sm">
-			Placeholder
-		</article>
-		<article class="col-sm">
-			Placeholder
-		</article>
-		<article class="col-sm">
-			Placeholder
-		</article>
-		<article class="col-sm">
-			Placeholder
-		</article>
-	</section>
-</article>
+<?php
+$post_types = [ 'ai1ec' ];
+$limit      = 4;
+; ?>
+<h3>Related Events</h3>
+@include('partials.content-related')
