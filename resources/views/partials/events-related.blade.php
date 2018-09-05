876201eff4cd7688d1415d1ed8fc9b9ea84e13e5
bdolor <bpayne@bccampus.ca>
2018-09-05 13:04:10 -0700

    clean up related events

--- resources/views/partials/events-related.blade.php
+++ resources/views/partials/events-related.blade.php
@@ -1,6 +1,21 @@
 <?php
 $post_types = [ 'ai1ec_event' ];
-$limit      = 4;
+$limit = 4;
 ; ?>
-<h3>Related Events</h3>
-@include('partials.content-related')
+<h3>Related Events <img src="@asset('images/green-dots.png')" alt="decorative green dots">
+	<small><a href="/calendar">view all events</a></small>
+</h3>
+<section class="relevant d-flex flex-row flex-wrap">
+	@foreach(\App\App::getRelevant($post, $post_types, $limit, $tag) as $related_post )
+		<?php
+		// not using $child->guid since guid does not
+		// update to current domain when importing content
+		$link = site_url() . '/' . $related_post->post_name;
+		$date = date( 'M d, Y', strtotime( $related_post->post_date ) );
+		;?>
+		<article class="col-3 border mx-2 p-2">
+			<p class="text-left upper">{{$date}}</p>
+			<h4 class="text-left"><a class="purple" href="<?php echo $link; ?>">{{$related_post->post_title}}</a></h4>
+		</article>
+	@endforeach
+</section>
