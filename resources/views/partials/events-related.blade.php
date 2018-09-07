07c6e4073ab4659a587daa21379326462f4bef23
bdolor <bpayne@bccampus.ca>
2018-09-07 12:57:24 -0700

    related events padding and margin

--- resources/views/partials/events-related.blade.php
+++ resources/views/partials/events-related.blade.php
@@ -13,7 +13,7 @@ $limit = 4;
 		$link = site_url() . '/' . $related_post->post_name;
 		$date = date( 'M d, Y', strtotime( $related_post->post_date ) );
 		;?>
-		<article class="col-3 border py-2">
+		<article class="col border py-2 m-md-2">
 			<p class="text-left upper">{{$date}}</p>
 			<h4 class="text-left"><a class="purple" href="{{$link}}">{{$related_post->post_title}}</a></h4>
 		</article>
