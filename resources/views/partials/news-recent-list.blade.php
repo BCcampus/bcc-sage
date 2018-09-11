f9f3924e101113efa6939c88f3384b034afa5ba5
Brad <brad@bradpayne.ca>
2018-09-11 10:03:00 -0700

    Recent news (#178)
    * fix featured news on homepage, news template
    
    * fix for related news component
    
    * add recent news component
    
    * add view all news link

--- resources/views/partials/news-recent-list.blade.php
+++ resources/views/partials/news-recent-list.blade.php
@@ -1,9 +1,9 @@
-<div class="row-fluid">
+<section class="row-fluid recent-news">
 	<h4 class="pad">Recent News</h4>
 	<ul>
 		<?php
 		$args = [
-			'posts_per_page' => 5
+			'posts_per_page' => 3
 		];?>
 		@foreach(\App\App::getLatestNews( $args ) as $news)
 			<?php
@@ -12,10 +12,13 @@
 			$link = site_url() . '/' . $news->post_name;
 			$date = date( 'M d, Y', strtotime( $news->post_date ) );
 			;?>
-			<li>
-				<p class="text-left upper">{{$date}}</p>
+			<li class="border-top">
+				<p class="upper">{{$date}}</p>
 				<p><a href="{{$link}}" rel="bookmark" title="{{$news->post_title}}">{{$news->post_title}}</a></p>
 			</li>
 		@endforeach
+		<li class="border-top">
+			<p><a href="<?php get_site_url(); ?>/bccampus-news">View all recent news</a></p>
+		</li>
 	</ul>
-</div>
+</section>
