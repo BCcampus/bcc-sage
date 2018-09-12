7657158dbb36c3c694ed4de6fd0a8fd3820249eb
Brad <brad@bradpayne.ca>
2018-09-12 11:29:37 -0700

    update copy in subscribe block (#183)

--- resources/views/partials/subscribe.blade.php
+++ resources/views/partials/subscribe.blade.php
@@ -1,6 +1,6 @@
 <section class="subscribe-box">
-	<h4>Subscribe to BCcampus News</h4>
-	<p>Get the latest information on news and events at BCcampus</p>
+	<h4>Subscribe to Our Newsletter</h4>
+	<p>Get the latest information on news and events at BCcampus.</p>
 	<form action="https://confirmsubscription.com/h/d/97F887551C7932F2">
 	<button name="subscribe" type="submit" class="btn btn-primary" formtarget="_blank">Subscribe</button>
 	</form>
