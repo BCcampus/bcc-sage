05b49882cb1970c71c71f6ed82a02f8c82f5b30e
alex-418 <aparedes@bccampus.ca>
2018-09-12 13:40:53 -0700

    update events template landing structure, make work well on mobile, add links

--- resources/views/template-events.blade.php
+++ resources/views/template-events.blade.php
@@ -13,22 +13,14 @@
         @include('partials.content-page')
     </div>
 
-    <section class="featured-events-front d-flex flex-row flex-wrap">
-        <article class="events-box-md col-sm d-flex">
-            <div class="featured-event col-sm d-flex"
-                 style="background-image: url({{get_stylesheet_directory_uri() . '/assets/images/placeholder-image-300x200.jpg'}});">
-                <h4 class="purple-bkgd col-sm mt-auto">Edtech Demos</div>
-        </article>
-        <article class="events-box-md col-sm d-flex">
-            <div class="featured-event col-sm d-flex"
-                 style="background-image: url({{get_stylesheet_directory_uri() . '/assets/images/placeholder-image-300x200.jpg'}});">
-                <h4 class="purple-bkgd col-sm mt-auto">Other Demos</h4></div>
-        </article>
-    </section>
+    <div class="container">
+        <div class="row">
+        <section class="featured-events-front d-flex flex-column col-sm">
+            <div class="featured-event col-sm d-flex" style="background-image: url({{get_stylesheet_directory_uri() . '/assets/images/placeholder-image-300x200.jpg'}});">
+                <h4 class="purple-bkgd col-sm mt-auto">Edtech Demos</h4>
+            </div>
 
-    <section class="featured-events-front d-flex flex-row flex-wrap">
-        <article class="events-box-md col-sm d-flex">
-            <ul class="events-list col-sm">
+            <ul class="events-list">
                 @foreach(\App\App::getEvents( 'edtech', $args ) as $recent )
                     @php($date = date( 'M d, Y', strtotime( $recent->post_date ) ))
                     <li class="border">
@@ -39,36 +31,32 @@
                     </li>
                 @endforeach
             </ul>
-        </article>
-
-        <article class="events-box-md col-sm d-flex">
-            <ul class="events-list col-sm">
-                @foreach(\App\App::getEvents( 'fol', $args ) as $recent )
-                    @php($date = date( 'M d, Y', strtotime( $recent->post_date ) ))
-                    <li class="border">
-                        <p class="upper">{{$date}}</p>
-                        <p><a href="{{$link}}" rel="bookmark"
-                              title="{{$recent->post_title}}">{{$recent->post_title}}</a>
-                        </p>
-                    </li>
-                @endforeach
-            </ul>
-        </article>
+        <h4>
+            <small><a href="/events_categories/edtech">EdTech Archives</a></small>
+        </h4>
     </section>
 
-    <section class="featured-events-front d-flex flex-row flex-wrap">
-        <div class="col-sm d-flex">
-            <h4>
-                <small><a href="/events_categories/edtech">EdTech Archives</a></small>
-            </h4>
+    <section class="featured-events-front d-flex flex-column col-sm">
+        <div class="featured-event col-sm d-flex" style="background-image: url({{get_stylesheet_directory_uri() . '/assets/images/placeholder-image-300x200.jpg'}});">
+            <h4 class="purple-bkgd col-sm mt-auto">FOL</h4>
         </div>
-        <div class="col-sm d-flex">
-            <h4>
-                <small><a href="/events_categories/fol">FOL Archives</a></small>
-            </h4>
-        </div>
+
+        <ul class="events-list">
+            @foreach(\App\App::getEvents( 'edtech', $args ) as $recent )
+                @php($date = date( 'M d, Y', strtotime( $recent->post_date ) ))
+                <li class="border">
+                    <p class="upper">{{$date}}</p>
+                    <p><a href="{{$link}}" rel="bookmark"
+                          title="{{$recent->post_title}}">{{$recent->post_title}}</a>
+                    </p>
+                </li>
+            @endforeach
+        </ul>
+        <h4>
+            <small><a href="/events_categories/fol">FOL Archives</a></small>
+        </h4>
     </section>
-
-
+        </div>
+    </div>
     @endwhile
 @endsection
