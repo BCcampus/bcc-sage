71144ed76268c23f21be7897ba50d5c8f61c3ea9
alex-418 <aparedes@bccampus.ca>
2018-09-14 13:09:30 -0700

    adds subscription page template and partial, update parent.blade to exlude related on it

--- resources/views/partials/subscribe.blade.php
+++ resources/views/partials/subscribe.blade.php
@@ -1,7 +1,8 @@
 <section class="subscribe-box">
 	<h4>Subscribe to Our Newsletter</h4>
 	<p>Get the latest information on news and events at BCcampus.</p>
-	<form action="https://confirmsubscription.com/h/d/97F887551C7932F2">
+	@php($link = site_url() . '/subscribe')
+	<form action="{{$link}}">
 	<button name="subscribe" type="submit" class="btn btn-primary" formtarget="_blank">Subscribe</button>
 	</form>
 </section>
