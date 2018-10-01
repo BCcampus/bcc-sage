69415917f54453dd8d01d87629665a23a772cf2c
bdolor <bpayne@bccampus.ca>
2018-10-01 16:26:19 -0700

    homepage left-column

--- resources/views/partials/children-projects-list.blade.php
+++ resources/views/partials/children-projects-list.blade.php
@@ -2,8 +2,8 @@
 	<div class="featured-image-box" style="background-image: url('@asset('images/placeholder-image-300x200.jpg')');">
 		<h5 class="blue-bkgd">Projects</h5>
 	</div>
-	<p>We actively participate in opportunities to improve the student experience in B.C. by facilitating technology advancements; enabling research activities; creating collaborative spaces; and more.</p>
-	<ul>
+	<p class="mt-3">We actively participate in opportunities to improve the student experience in B.C. by facilitating technology advancements; enabling research activities; creating collaborative spaces; and more.</p>
+	<ul class="mb-3">
 		@foreach(\App\Page::getChildrenOfPage($get_projects_id) as $child)
 			<?php
 			// not using $child->guid since guid does not
@@ -11,7 +11,7 @@
 			$link = site_url() . '/' . $child->post_name;
 			;
 			?>
-			<li class="border"><a class="purple" href="{{$link}}">{{$child->post_title}}<i class="fa fa-angle-right pull-right"></i></a></li>
+			<li class="border-top border-right border-left border-last"><a class="purple font-weight-bold" href="{{$link}}">{{$child->post_title}}<i class="fa fa-arrow-right pull-right"></i></a></li>
 		@endforeach
 	</ul>
 </section>
--- resources/views/partials/children-top-list.blade.php
+++ resources/views/partials/children-top-list.blade.php
@@ -2,15 +2,15 @@
 	<div class="featured-image-box" style="background-image: url('@asset('images/placeholder-image-300x200.jpg')');">
 		<h5 class="blue-bkgd">Topics of Practice</h5>
 	</div>
-	<p>At BCcampus, we support the adaptation and evolution of teaching and learning practices in post-secondary institutions across British Columbia through collaboration, communication, and innovation.</p>
-	<ul>
+	<p class="mt-3">At BCcampus, we support the adaptation and evolution of teaching and learning practices in post-secondary institutions across British Columbia through collaboration, communication, and innovation.</p>
+	<ul class="mb-3">
 		@foreach(\App\Page::getChildrenOfPage($get_topics_of_practice_id) as $child)
 			<?php
 			// not using $child->guid since guid does not d
 			// update to current domain when importing content
 			$link = site_url() . '/' . $child->post_name;
 			;?>
-			<li class="border"><a class="purple" href="{{$link}}">{{$child->post_title}}<i class="fa fa-angle-right pull-right"></i></a></li>
+			<li class="border-top border-right border-left border-last"><a class="purple font-weight-bold" href="{{$link}}">{{$child->post_title}}<i class="fa fa-arrow-right pull-right"></i></a></li>
 		@endforeach
 	</ul>
 </section>
