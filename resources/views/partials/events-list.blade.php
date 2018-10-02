0490525b23ab50246883b030f79fce66e8eb04b5
bdolor <bpayne@bccampus.ca>
2018-10-02 14:44:14 -0700

    purple links, bs4 spacing declarations

--- resources/views/partials/events-list.blade.php
+++ resources/views/partials/events-list.blade.php
@@ -2,9 +2,9 @@
 	@foreach(\App\App::getEvents( $event_category, $args ) as $recent )
 		@php($link = site_url() . '/' . $recent->post_name)
 		@php($date = date( 'M d, Y', strtotime( $recent->post_date ) ))
-		<li class="border">
+		<li class="border my-2 p-2">
 			<p class="text-uppercase"><time>{{$date}}</time></p>
-			<p><a href="{{$link}}" rel="bookmark"
+			<p class="purple font-weight-bold"><a href="{{$link}}" rel="bookmark"
 				  title="{{$recent->post_title}}">{{$recent->post_title}}</a>
 			</p>
 		</li>
