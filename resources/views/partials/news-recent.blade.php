08673940700b62df4dd8dec5a40a825602e02641
bdolor <bpayne@bccampus.ca>
2018-10-02 11:18:33 -0700

    recent news tweaks

--- resources/views/partials/news-recent.blade.php
+++ resources/views/partials/news-recent.blade.php
@@ -4,18 +4,23 @@ $args = [
 	'category'       => 0,
 ];
 @endphp
-<section class="recent-news pad-top d-flex flex-row flex-wrap">
+<section class="recent-news pad-top d-flex flex-row flex-wrap justify-content-start">
 	@foreach(\App\App::getLatestNews( $args ) as $recent )
 		@php($link = site_url() . '/' . $recent->post_name)
-			<article class="col feature-box-sm border" itemscope itemtype="http://schema.org/Article">
+			<article class="col feature-box-sm" itemscope itemtype="http://schema.org/Article">
 				<a href="{{$link}}">
 					<div class="featured-image-box" style="background-image: url({{\App\App::getThumbUrl($recent->ID)}});">
 
 					</div>
 				</a>
-				<p class="upper pt-2 px-3"><time itemprop="datePublished" class="updated" datetime="{{ get_post_time('c', true, $recent->ID) }}">{{ get_the_date('',$recent->ID) }}</time></p>
-				<h4><a class="purple" href="{{$link}}">{{$recent->post_title}}</a>
-				</h4>
+				<div class="border min-height-sm">
+					<p class="upper pt-2 px-3">
+						<time itemprop="datePublished" class="updated"
+							  datetime="{{ get_post_time('c', TRUE, $recent->ID) }}">{{ get_the_date('',$recent->ID) }}</time>
+					</p>
+					<h4><a class="purple" href="{{$link}}">{{$recent->post_title}}</a>
+					</h4>
+				</div>
 			</article>
 	@endforeach
 </section>
