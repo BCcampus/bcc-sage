f7dd789129748aea5ad096ee94ce79e3d278efd2
bdolor <bpayne@bccampus.ca>
2018-10-04 14:06:28 -0700

    padding fiddle

--- resources/views/partials/children-grants.blade.php
+++ resources/views/partials/children-grants.blade.php
@@ -1,12 +1,12 @@
-<h4 class="pt-5">Grants Currently Offered</h4>
+<h4 class="pt-4 pb-2">Grants Currently Offered</h4>
 <section class="grants d-flex flex-row flex-wrap">
 	@foreach(\App\Page::getChildrenOfPage() as $child)
 		@php
 			$link = site_url() . '/' . $child->post_name;
 		@endphp
-		<article class="grants-current col-md-6 mb-3" itemscope itemtype="http://schema.org/Article">
+		<article class="grants-current col-md-6 mb-2" itemscope itemtype="http://schema.org/Article">
 			<div class="featured-grant row-fluid d-flex" style="background-image: url({{\App\App::getThumbUrl($child->ID)}});">
-				<h4 class="purple-bkgd text-inverse col-sm mt-auto"><a href="{{$link}}">{{$child->post_title}}</a>
+				<h4 class="text-center purple-bkgd text-inverse col-sm mt-auto"><a href="{{$link}}">{{$child->post_title}}</a>
 				</h4>
 			</div>
 			<div class="row-fluid border-left border-right border-bottom min-height-md">
