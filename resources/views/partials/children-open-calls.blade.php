81a523ff59a6c6586d210cf69e8b20090ff9be2e
bdolor <bpayne@bccampus.ca>
2018-10-04 14:16:42 -0700

    grants open closed parity

--- resources/views/partials/children-open-calls.blade.php
+++ resources/views/partials/children-open-calls.blade.php
@@ -1,25 +1,25 @@
 <h4 class="pt-5">Open Calls for Proposals</h4>
-<section class="grants-open d-flex flex-row flex-wrap">
+<section class="grants d-flex flex-row flex-wrap">
 	@php
 		$parent = get_theme_mod('grants_open_setting', '' );
 	@endphp
 	@if ($parent !== '')
-			@foreach(\App\Page::getChildrenOfPage( $parent ) as $child)
-				@php
-					$link = site_url() . '/' . $child->post_name;
-				@endphp
-				<article class="grants-open col-sm-6 no-gutters mb-2 px-2" itemscope
-						 itemtype="http://schema.org/Article">
-					<div class="featured-grant row-fluid d-flex"
-						 style="background-image: url({{\App\App::getThumbUrl($child->ID)}});">
-						<h4 class="purple-bkgd text-inverse col-sm mt-auto"><a
-								href="{{$link}}">{{$child->post_title}}</a>
-						</h4>
-					</div>
-					<div class="row-fluid border-left border-right border-bottom">
-						<p class="pt-3 pad-left pad-right"><?php echo wp_trim_words( $child->post_content, '30', "<a href='{$link}'>&hellip;<i class='fa fa-arrow-right'></i></a>" );?></p>
-					</div>
-				</article>
-			@endforeach
+		@foreach(\App\Page::getChildrenOfPage( $parent ) as $child)
+			@php
+				$link = site_url() . '/' . $child->post_name;
+			@endphp
+			<article class="grants-open col-md-6 mb-2 px-2" itemscope
+					 itemtype="http://schema.org/Article">
+				<div class="featured-grant row-fluid d-flex"
+					 style="background-image: url({{\App\App::getThumbUrl($child->ID)}});">
+					<h4 class="text-center purple-bkgd text-inverse col-sm mt-auto"><a
+							href="{{$link}}">{{$child->post_title}}</a>
+					</h4>
+				</div>
+				<div class="row-fluid border min-height-md">
+					<p class="pt-3 px-2"><?php echo wp_trim_words( $child->post_content, '30', "<a href='{$link}'>&hellip;<i class='fa fa-arrow-right'></i></a>" );?></p>
+				</div>
+			</article>
+		@endforeach
 	@endif
 </section>
