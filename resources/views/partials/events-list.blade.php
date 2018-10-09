1943f985c917505059e3b7692340fe5ec30e413e
bdolor <bpayne@bccampus.ca>
2018-10-09 15:50:11 -0700

    fix link value for events list

--- resources/views/partials/events-list.blade.php
+++ resources/views/partials/events-list.blade.php
@@ -1,9 +1,11 @@
 <ul class="events-list">
 	@foreach(\App\App::getUpcomingEvents( $limit, $filter ) as $recent )
 		<li class="border my-2 p-2">
-			<p class="text-uppercase font-size-sm"><time>{{$recent['start']}}</time></p>
-			<p class="font-weight-bold"><a class="purple" href="{{$link}}" rel="bookmark"
-				  title="{{$recent['title']}}">{{$recent['title']}}</a>
+			<p class="text-uppercase font-size-sm">
+				<time>{{$recent['start']}}</time>
+			</p>
+			<p class="font-weight-bold"><a class="purple" href="{{$recent['link']}}" rel="bookmark"
+										   title="{{$recent['title']}}">{{$recent['title']}}</a>
 			</p>
 		</li>
 	@endforeach
