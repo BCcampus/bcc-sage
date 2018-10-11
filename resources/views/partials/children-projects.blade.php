7c61aa9885b68cd9880fddf711882b482370548e
bdolor <bpayne@bccampus.ca>
2018-10-11 11:30:33 -0700

    fix padding mobile current projects

--- resources/views/partials/children-projects.blade.php
+++ resources/views/partials/children-projects.blade.php
@@ -8,7 +8,7 @@
 		<article class="projects flex-row d-flex flex-wrap my-2" itemscope itemtype="http://schema.org/Article">
 			<div class="col-md-3 events-image-box" style="background-image: url({{\App\App::getThumbUrl( $child->ID )}});">
 			</div>
-			<div class="col-md-9">
+			<div class="col-md-9 px-md-3 px-0">
 				<h5 class="pt-2"><a class="purple" href="{{$link}}">{{$child->post_title}}</a></h5>
 				<p><?php echo wp_trim_words( $child->post_content, '30', "<a href='{$link}'>&hellip;</a>" ) ;?></p>
 			</div>
