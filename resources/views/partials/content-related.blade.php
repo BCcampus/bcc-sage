d244c9486b221919c5d7c48fb1b9db2c7a9611f0
bdolor <bpayne@bccampus.ca>
2018-10-11 11:58:24 -0700

    handle html entities properly

--- resources/views/partials/content-related.blade.php
+++ resources/views/partials/content-related.blade.php
@@ -7,10 +7,9 @@
 		$cat = get_the_category( $related_post->ID );
 		;?>
 		<article class="col feature-box-sm py-2" itemscope itemtype="http://schema.org/Article">
-			<a href="{{$link}}">
-				<div class="featured-image-box" style="background-image: url({{\App\App::getThumbUrl($related_post->ID)}});">
-				</div>
-			</a>
+			<div class="featured-image-box"
+				 style="background-image: url({{\App\App::getThumbUrl($related_post->ID)}});">
+			</div>
 			<div class="border min-height-md">
 				<p class="text-uppercase pt-2 px-3 font-size-sm">
 					<time itemprop="datePublished"
@@ -18,7 +17,8 @@
 				</p>
 				<h4><a class="purple" href="{{$link}}">{{$related_post->post_title}}</a>
 				</h4>
-				<p class="px-3 font-size-sm text-uppercase mb-1 font-weight-500"><a href="<?php echo esc_url( get_category_link( $cat[0]->term_id ) );?>">{{$cat[0]->name}}</a></p>
+				<p class="px-3 font-size-sm text-uppercase mb-1 font-weight-500"><a
+						href="<?php echo esc_url( get_category_link( $cat[0]->term_id ) );?>">{{wp_specialchars_decode($cat[0]->name)}}</a></p>
 			</div>
 		</article>
 	@endforeach
