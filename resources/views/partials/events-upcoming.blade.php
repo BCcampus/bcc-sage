c777df41e878c1e112abb03ceb30dc193f60246a
bdolor <bpayne@bccampus.ca>
2018-10-11 10:57:33 -0700

    fix mobile view upcoming events

--- resources/views/partials/events-upcoming.blade.php
+++ resources/views/partials/events-upcoming.blade.php
@@ -7,19 +7,17 @@
 		<h5 class="text-uppercase">Upcoming</h5>
 	</header>
 	@foreach(\App\App::getUpcomingEvents( $limit, $ids ) as $recent )
-		<article class="events-upcoming my-1 border" itemscope itemtype="http://schema.org/Article">
-			<div class="row pad-left">
-				<div class="col-sm-2 events-image-box"
+		<article class="events-upcoming my-2 border flex-row d-flex" itemscope itemtype="http://schema.org/Article">
+				<div class="col-md-3 events-image-box"
 					 style="background-image: url({{ \App\App::getThumbUrl($recent['post_id']) }});">
 				</div>
-				<div class="col-sm-10">
+				<div class="col-md-9">
 					<p class="text-uppercase pt-2 font-size-sm">
 						<time>{{ $recent['start'] }}</time>
 					</p>
 					<h4><a class="purple" href="@php echo esc_url( $recent['link'] ); @endphp">{{ $recent['title']}}</a>
 					</h4>
 				</div>
-			</div>
 		</article>
 	@endforeach
 </section>
