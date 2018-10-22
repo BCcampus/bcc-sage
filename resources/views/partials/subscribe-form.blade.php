5f912c788627b0e33e36c7477e425a6030252a66
bdolor <bpayne@bccampus.ca>
2018-10-22 10:10:04 -0700

    add edtech subscribe form and template

--- resources/views/partials/subscribe-form.blade.php
+++ resources/views/partials/subscribe-form.blade.php
@@ -1,24 +1,26 @@
 <section class="subscribe-form form-group">
-	<form id="subForm" class="js-cm-form col-sm-6" action="https://www.createsend.com/t/subscribeerror?description=" method="post" data-id="92D4C54F0FEC16E5ADC2B1904DE9ED1A9335FD31A103A8FD0E4D80F7D0782DDB61AB787B97D7DE5AAFA39E83D0AFEAC38493B9A61A2E52DEC7F204365A64BAFA">
+	<form id="subForm" class="js-cm-form col-sm-6" action="https://www.createsend.com/t/subscribeerror?description="
+		  method="post"
+		  data-id="92D4C54F0FEC16E5ADC2B1904DE9ED1A9335FD31A103A8FD0E4D80F7D0782DDB61AB787B97D7DE5AAFA39E83D0AFEAC38493B9A61A2E52DEC7F204365A64BAFA">
 		<p>
 			<label for="fielddudue">First Name</label>
-			<br />
-			<input id="fielddudue" class="form-control" name="cm-f-dudue" type="text" />
+			<br/>
+			<input id="fielddudue" class="form-control" name="cm-f-dudue" type="text"/>
 		</p>
 		<p>
 			<label for="fielddudug">Last Name</label>
-			<br />
-			<input id="fielddudug" class="form-control" name="cm-f-dudug" type="text" />
+			<br/>
+			<input id="fielddudug" class="form-control" name="cm-f-dudug" type="text"/>
 		</p>
 		<p>
 			<label for="fieldEmail">Email</label>
-			<br />
+			<br/>
 			<input id="fieldEmail" name="cm-jrkudj-jrkudj" type="email" class="js-cm-email-input form-control"
-				   required />
+				   required/>
 		</p>
 		<p>
 			<label>What area(s) are you interested in learning more about?</label>
-			<br />
+			<br/>
 		<div>
 			<input id="fielddklkx-0" name="cm-fo-dklkx" type="checkbox" value="450783"
 			/>
@@ -57,7 +59,7 @@
 		</p>
 		<p>
 			<label>What is your role?</label>
-			<br />
+			<br/>
 		<div>
 			<input id="fielddklkm-0" name="cm-fo-dklkm" type="checkbox" value="450787"
 			/>
@@ -86,8 +88,8 @@
 		</p>
 		<p>
 			<label for="fielddklkq">If you selected 'Other' above, please describe your role.</label>
-			<br />
-			<input id="fielddklkq" class="form-control" name="cm-f-dklkq" type="text" />
+			<br/>
+			<input id="fielddklkq" class="form-control" name="cm-f-dklkq" type="text"/>
 		</p>
 		<p>
 			<button class="js-cm-submit-button btn btn-primary" type="submit">Subscribe</button>
---
+++ resources/views/template-subscribe-edtech.blade.php
@@ -0,0 +1,9 @@
+{{--
+  Template Name: EdTech Subscribe Landing Template
+--}}
+
+@extends('layouts.parent')
+
+@section('content')
+	@include('partials.subscribe-edtech-form')
+@endsection
