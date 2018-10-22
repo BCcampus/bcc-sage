71f864e2e6b6a28ba77f15dd5a8a7a97a80c6473
bdolor <bpayne@bccampus.ca>
2018-10-22 14:19:44 -0700

    escape nasty guid

--- resources/views/partials/children-open-calls.blade.php
+++ resources/views/partials/children-open-calls.blade.php
@@ -7,16 +7,17 @@
 		$last_day = 0;
 	@endphp
 	@foreach(\App\App::getUpcomingEvents( $limit, $ids, $last_day ) as $open)
+		@php($link=esc_url(get_permalink($open['post_id'])))
 		<article class="grants-open col-md-6 my-2" itemscope
 				 itemtype="http://schema.org/Article">
 			<div class="featured-grant row-fluid d-flex"
 				 style="background-image: url({{\App\App::getThumbUrl($open['post_id'])}});">
 				<h4 class="text-center purple-bkgd text-inverse col-sm mt-auto"><a
-						href="{{$open['link']}}">{{wp_specialchars_decode($open['title'])}}</a>
+						href="{{$link}}">{{wp_specialchars_decode($open['title'])}}</a>
 				</h4>
 			</div>
 			<div class="row-fluid border min-height-md">
-				<p class="pt-3 px-2"><?php echo wp_trim_words( $open['post_content'], '30', "<a href='{$open['link']}'>&hellip;<i class='fa fa-arrow-right'></i></a>" );?></p>
+				<p class="pt-3 px-2"><?php echo wp_trim_words( $open['post_content'], '30', "<a href='{$link}'>&hellip;<i class='fa fa-arrow-right'></i></a>" );?></p>
 			</div>
 		</article>
 	@endforeach
--- resources/views/partials/events-list.blade.php
+++ resources/views/partials/events-list.blade.php
@@ -1,10 +1,11 @@
 <ul class="events-list">
 	@foreach(\App\App::getUpcomingEvents( $limit, $ids ) as $recent )
+		@php($link=esc_url(get_permalink($recent['post_id'])))
 		<li class="border my-2 p-2">
 			<p class="text-uppercase font-size-sm">
 				<time>{{$recent['start']}}</time>
 			</p>
-			<p class="font-weight-bold"><a class="purple" href="{{$recent['link']}}" rel="bookmark"
+			<p class="font-weight-bold"><a class="purple" href="{{$link}}" rel="bookmark"
 										   title="{{$recent['title']}}">{{$recent['title']}}</a>
 			</p>
 		</li>
