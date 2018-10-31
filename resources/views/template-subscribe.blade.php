92c15225f6e6ce5770c3fdc5011c3c8d2ca8e0c3
bdolor <bpayne@bccampus.ca>
2018-10-31 14:13:31 -0700

    update subscribe form templates

--- resources/views/template-subscribe.blade.php
+++ resources/views/template-subscribe.blade.php
@@ -5,5 +5,8 @@
 @extends('layouts.parent')
 
 @section('content')
+	@while(have_posts()) @php(the_post())
+	@include('partials.content-page')
     @include('partials.subscribe-form')
+	@endwhile
 @endsection
