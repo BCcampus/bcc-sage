b06b35c3b5c94e805b5df06fdfbcc41885b9b69a
bdolor <bpayne@bccampus.ca>
2018-11-01 14:11:59 -0700

    top image as links

--- resources/views/partials/children-top.blade.php
+++ resources/views/partials/children-top.blade.php
@@ -2,13 +2,17 @@
 	@foreach(\App\Page::getChildrenOfPage() as $child)
 		@php($link=\App\App::maybeGuid($child->ID, $child->post_name))
 		<article class="topics-of-practice col-sm-6 no-gutters mb-2 px-2" itemscope itemtype="http://schema.org/Article">
+			<a href="{{$link}}" class="img-link">
 			<div class="featured-topic row-fluid d-flex" style="background-image: url({{\App\App::getThumbUrl($child->ID)}});">
-				<h4 class="purple-bkgd text-inverse col-sm mt-auto"><a href="{{$link}}">{{wp_specialchars_decode($child->post_title)}}</a>
+				<h4 itemprop="name" class="purple-bkgd text-inverse col-sm mt-auto">{{wp_specialchars_decode($child->post_title)}}
 				</h4>
 			</div>
+			</a>
 			<div class="row-fluid border-left border-right border-bottom">
 				<p class="pt-3 pad-left pad-right"><?php echo wp_trim_words( $child->post_content, '25', "<a href='{$link}'>&hellip;<i class='fa fa-arrow-right'></i></a>" ); ?></p>
 			</div>
+			<meta itemprop="author" content="{{get_the_author_meta('display_name',$child->post_author)}}"/>
+			<meta itemprop="image" content="{{\App\App::getThumbUrl($child->ID)}}"/>
 		</article>
 	@endforeach
 </section>
