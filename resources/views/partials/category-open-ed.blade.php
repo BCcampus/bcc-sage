0fb53db64405b6265a9a133d593ee1e145e418f9
bdolor <bpayne@bccampus.ca>
2018-11-02 09:43:00 -0700

    alignment clean up on mobile

--- resources/views/partials/category-open-ed.blade.php
+++ resources/views/partials/category-open-ed.blade.php
@@ -2,7 +2,7 @@
 $post_types = [ 'post' ];
 $limit      = 4;
 $category_name = 'Open Education'; ?>
-<h3>Open Education <img class="mx-2 mb-1" src="@asset('images/green-dots.png')" alt="decorative green dots">
+<h3 class="mt-2">Open Education <img class="mx-2 mb-1" src="@asset('images/green-dots.png')" alt="decorative green dots">
 	<small><a href="{{get_site_url()}}/category/open-education/">view all Open Education news</a></small>
 </h3>
 @include('partials.content-related')
