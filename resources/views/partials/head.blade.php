1a6399af1035102e0953a8d848eaf6596c6e2201
bdolor <bpayne@bccampus.ca>
2018-11-20 11:01:35 -0800

    add site verification

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,9 +1,10 @@
 <head>
-  <meta charset="utf-8"/>
-  <meta http-equiv="x-ua-compatible" content="ie=edge"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  @foreach($get_micro_data as $itemprop => $content)
-    <meta itemprop="{{$itemprop}}" content="{{$content}}" id="{{$itemprop}}"/>
-  @endforeach
-  @php(wp_head())
+	<meta charset="utf-8"/>
+	<meta http-equiv="x-ua-compatible" content="ie=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<meta name="google-site-verification" content="IQ5X6Lwp0QSIiwWqxeZHv8v7D30eoVamvKnDF66-6ck"/>
+	@foreach($get_micro_data as $itemprop => $content)
+		<meta itemprop="{{$itemprop}}" content="{{$content}}" id="{{$itemprop}}"/>
+	@endforeach
+	@php(wp_head())
 </head>
