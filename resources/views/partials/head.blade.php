a9aeafb9b1842ffd493cc1c331ea379b0e1f81da
alex-418 <aparedes@bccampus.ca>
2019-01-23 15:21:39 -0800

    update head.blade.php to include favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 	<meta name="google-site-verification" content="IQ5X6Lwp0QSIiwWqxeZHv8v7D30eoVamvKnDF66-6ck"/>
+	<link rel="shortcut icon" href="@asset('images/favicon.ico')" />
 	@foreach($get_micro_data as $itemprop => $content)
 		<meta itemprop="{{$itemprop}}" content="{{$content}}" id="{{$itemprop}}"/>
 	@endforeach
