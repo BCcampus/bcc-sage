cf43abd149a8ad856494f5f7cb82330038b64eb8
bdolor <brad@bradpayne.ca>
2019-01-23 14:19:20 -0800

    prefer a sort order of most recent, fix php warnings

--- resources/views/partials/events-related.blade.php
+++ resources/views/partials/events-related.blade.php
@@ -1,13 +1,14 @@
 @php
 	$post_types = [ 'ai1ec_event' ];
 	$limit = 4;
+	$category_name = '';
 @endphp
 <div class="container-fluid my-3">
 <h3>Related Events <img class="mx-2 mb-1" src="@asset('images/green-dots.png')" alt="decorative green dots">
 	<small><a href="{{get_site_url()}}/events">view all events</a></small>
 </h3>
 <section class="related-events d-flex flex-row flex-wrap">
-	@foreach(\App\App::getRelevant($post, $post_types, $limit, $tag) as $related_post )
+	@foreach(\App\App::getRelevant($post, $post_types, $limit, $category_name) as $related_post )
 		<article class="col-md-3 my-2" itemscope itemtype="http://schema.org/Event">
 			<div class="border p-3 min-height-sm">
 			<p class="text-uppercase font-size-sm"><time itemprop="startDate" datetime="{{ get_post_time('c', true, $related_post->ID) }}">{{ get_the_date('',$related_post->ID) }}</time></p>
