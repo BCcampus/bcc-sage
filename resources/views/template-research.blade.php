c6f98629a1925f514851e81947606385801a1ca5
alex-418 <aparedes@bccampus.ca>
2019-01-23 14:31:56 -0800

    add research landing page

---
+++ resources/views/template-research.blade.php
@@ -0,0 +1,11 @@
+{{--
+  Template Name: Research Landing Template
+--}}
+
+@extends('layouts.parent')
+
+@section('content')
+	@while(have_posts()) @php(the_post())
+	@include('partials.content-page')
+	@endwhile
+@endsection
