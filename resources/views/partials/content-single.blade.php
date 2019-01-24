f42fbf7ef28510ac0466496115ec6d968b34b141
bdolor <brad@bradpayne.ca>
2019-01-23 16:09:22 -0800

    add ai1ec_event single event template

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -9,39 +9,31 @@
 	<header class="entry-header">
 		<h2 itemprop="name">{!! get_the_title() !!}</h2>
 	</header>
-	@if ( !( $post->post_type === 'ai1ec_event' ) )
-		<p class="byline author vcard text-uppercase">
-			<small>
-				{{ __('By', 'bcc-sage') }}
-				<a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
+	<p class="byline author vcard text-uppercase">
+		<small>
+			{{ __('By', 'bcc-sage') }}
+			<a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
 				  <span itemprop="author" itemscope itemtype="http://schema.org/Person">
 					<span itemprop="name">{{ get_the_author() }}</span>
 				  </span>
-				</a>
-				&nbsp;<span class="fa fa-circle green"></span>&nbsp;
-				@include('partials.entry-meta')
-				&nbsp<span class="fa fa-circle green"></span>&nbsp;
-				<span itemprop="articleSection">{{ the_category( ', ' ) }}</span>
-			</small>
-		</p>
-	@endif
-	@if ( ( $post->post_type === 'ai1ec_event' ) )
-		<div itemprop="articleBody" class="entry-content ai1ec-single">
-			@else
-				<div itemprop="articleBody" class="entry-content">
-					@endif
-					@php(the_content())
-				</div>
-				<footer class="post-footer">
-					@if( !is_singular( 'ai1ec_event' ))
-						<?php
-						$post_types = [ 'post', 'page' ];
-						$limit      = 3;
-						;?>
-						<h3 class="pt-3">Related Stories <img class="mx-2 mb-1" src="@asset('images/green-dots.png')"
-															  alt="decorative green dots"></h3>
-						@include('partials.content-related')
-					@endif
-				</footer>
-		@php(comments_template('/partials/comments.blade.php'))
+			</a>
+			&nbsp;<span class="fa fa-circle green"></span>&nbsp;
+			@include('partials.entry-meta')
+			&nbsp<span class="fa fa-circle green"></span>&nbsp;
+			<span itemprop="articleSection">{{ the_category( ', ' ) }}</span>
+		</small>
+	</p>
+	<div itemprop="articleBody" class="entry-content">
+		@php(the_content())
+	</div>
+	<footer class="post-footer">
+		<?php
+		$post_types    = [ 'post', 'page' ];
+		$limit         = 3;
+		$category_name = '';
+		;?>
+		<h3 class="pt-3">Related Stories <img class="mx-2 mb-1" src="@asset('images/green-dots.png')" alt="decorative green dots"></h3>
+		@include('partials.content-related')
+	</footer>
+	@php(comments_template('/partials/comments.blade.php'))
 </article>
