27c155ed1bb16ac80561f4748748c97c7226bf20
alex-418 <aparedes@bccampus.ca>
2019-06-04 13:50:49 -0700

    filter out attachments from homepage child pages query

--- resources/views/partials/children-projects-list.blade.php
+++ resources/views/partials/children-projects-list.blade.php
@@ -7,8 +7,10 @@
 	<p class="mt-3">We actively participate in opportunities to improve the student experience in B.C. by facilitating technology advancements; enabling research activities; creating collaborative spaces; and more.</p>
 	<div class="mb-3 list-group">
 		@foreach(\App\Page::getChildrenOfPage($get_projects_id) as $child)
+			@if( $child->post_type != 'attachment')
 			@php($link=\App\App::maybeGuid($child->ID, $child->post_name))
 			<a class="list-group-item list-group-item-action purple font-weight-bold" href="{{$link}}">{{wp_specialchars_decode($child->post_title)}}<span class="fa fa-arrow-right pull-right"></span></a>
+		@endif
 		@endforeach
 	</div>
 </section>
--- resources/views/partials/children-top-list.blade.php
+++ resources/views/partials/children-top-list.blade.php
@@ -7,8 +7,10 @@
 	<p class="mt-3">At BCcampus, we support the adaptation and evolution of teaching and learning practices in post-secondary institutions across British Columbia through collaboration, communication, and innovation.</p>
 	<div class="mb-3 list-group">
 		@foreach(\App\Page::getChildrenOfPage($get_topics_of_practice_id) as $child)
+			@if( $child->post_type != 'attachment')
 			@php($link=\App\App::maybeGuid($child->ID, $child->post_name))
 			<a class="list-group-item list-group-item-action purple font-weight-bold" href="{{$link}}">{{wp_specialchars_decode($child->post_title)}}<span class="fa fa-arrow-right pull-right"></span></a>
-		@endforeach
+		@endif
+				@endforeach
 	</div>
 </section>
